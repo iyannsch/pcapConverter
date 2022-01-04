f4c617e45a2a43418d6556a80c6297f240163137
Prakita Renatin <ge25zow@mytum.de>
2022-01-04 18:48:47 +0100

    Changed format of capture.sh to comply with what main.py expects

--- capture.sh
+++ capture.sh
@@ -2,5 +2,7 @@
 # Script for cronjob. Captures 15 minutes of traffic and preprocesses it
 FILENAME=raw_$(date --iso-8601=seconds).pcap
 INTERFACE=interface # Change interface here
-timeout --preserve-status 15m tcpdump -i $INTERFACE -w $FILENAME \
-        && converter-script $FILENAME # TODO
+DURATION=900 # Duration in seconds
+PATH_JSONDB=db.json # Path to JSON db
+timeout --preserve-status $DURATION tcpdump -i $INTERFACE -w $FILENAME \
+        && python main.py $FILENAME $DURATION $PATH_JSONDB # TODO
