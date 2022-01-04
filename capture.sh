8a3d7ed370e8bb505905f687d9700994cc0b8133
Prakita Renatin <ge25zow@mytum.de>
2022-01-04 18:54:13 +0100

    Added data folder

--- capture.sh
+++ capture.sh
@@ -3,6 +3,6 @@
 FILENAME=raw_$(date --iso-8601=seconds).pcap
 INTERFACE=interface # Change interface here
 DURATION=900 # Duration in seconds
-PATH_JSONDB=db.json # Path to JSON db
+PATH_JSONDB=data/db.json # Path to JSON db
 timeout --preserve-status $DURATION tcpdump -i $INTERFACE -w $FILENAME \
-        && python main.py $FILENAME $DURATION $PATH_JSONDB # TODO
+        && python main.py $FILENAME $DURATION $PATH_JSONDB
