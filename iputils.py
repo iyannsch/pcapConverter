fd8f752aa88623927f764e2d4fe21e39d1725bf1
Yannik Schmidt <>
2022-01-08 11:53:41 +0100

    Added caching of cloud service provider ips for better performance

--- iputils.py
+++ iputils.py
@@ -22,36 +22,57 @@ class IPUtils:
     AZURE_IPV6.pop()
     file.close()
 
+    ip4_cache = {}
+    ip6_cache = {}
+
     def check_Google(self, ipadr, isV4):
         if(isV4 == True):
+            if(ipadr in self.ip4_cache and self.ip4_cache[ipadr] == "Google"):
+                return True
             for ipnet in self.GOOGLE_IPV4:
                 if(ipaddress.ip_address(ipadr) in ipaddress.ip_network(ipnet)):
+                    self.ip4_cache[ipadr] = "Google"
                     return True
         else:
+            if(ipadr in self.ip6_cache and self.ip6_cache[ipadr] == "Google"):
+                return True
             for ipnet in self.GOOGLE_IPV6:
                 if(ipaddress.ip_address(ipadr) in ipaddress.ip_network(ipnet)):
+                    self.ip6_cache[ipadr] = "Google"
                     return True
         return False
     
     def check_AWS(self, ipadr, isV4):
         if(isV4 == True):
+            if(ipadr in self.ip4_cache and self.ip4_cache[ipadr] == "AWS"):
+                return True
             for ipnet in self.AWS_IPV4:
                 if(ipaddress.ip_address(ipadr) in ipaddress.ip_network(ipnet)):
+                    self.ip4_cache[ipadr] = "AWS"
                     return True
         else:
+            if(ipadr in self.ip6_cache and self.ip6_cache[ipadr] == "AWS"):
+                return True
             for ipnet in self.AWS_IPV6:
                 if(ipaddress.ip_address(ipadr) in ipaddress.ip_network(ipnet)):
+                    self.ip6_cache[ipadr] = "AWS"
                     return True
         return False
 
     def check_Azure(self, ipadr, isV4):
         if(isV4== True):
+            if(ipadr in self.ip4_cache and self.ip4_cache[ipadr] == "Azure"):
+                return True
             for ipnet in self.AZURE_IPV4:
                 if(ipaddress.ip_address(ipadr) in ipaddress.ip_network(ipnet)):
+                    self.ip4_cache[ipadr] = "Azure"
                     return True
         else:
+            if(ipadr in self.ip6_cache and self.ip6_cache[ipadr] == "Azure"):
+                return True
             for ipnet in self.AZURE_IPV6:
                 if(ipaddress.ip_address(ipadr) in ipaddress.ip_network(ipnet)):
+                    self.ip6_cache[ipadr] = "Azure"
                     return True
         return False
     
