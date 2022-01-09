87fe4fc150385a6b66312e3854a5b541b14b71ba
Prakita Renatin <ge25zow@mytum.de>
2022-01-09 19:16:29 +0000

    Added snap length

--- capture.sh
+++ capture.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # Script for cronjob. Captures 15 minutes of traffic and preprocesses it
 FILENAME=raw_$(date --iso-8601=seconds).pcap
-INTERFACE=interface # Change interface here
+INTERFACE=wlan0 # Change interface here
 DURATION=900 # Duration in seconds
 PATH_JSONDB=data/db.json # Path to JSON db
-timeout --preserve-status $DURATION tcpdump -i $INTERFACE -w $FILENAME \
+SNAPLEN=500 # Snap Length
+timeout --preserve-status $DURATION tcpdump -i $INTERFACE -s $SNAPLEN -w $FILENAME \
         && python main.py $FILENAME $PATH_JSONDB $DURATION $INTERFACE
